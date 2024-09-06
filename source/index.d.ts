1db133eebe0dca2cb9fb4bfb03f31ff902bde2b6
ehmicky <ehmicky@users.noreply.github.com>
2024-09-06 16:15:48 +0100

    Add types (#70)

--- source/index.d.ts
+++ source/index.d.ts
@@ -1,12 +1,52 @@
-export type Options = {
-	readonly timeout: number;
-	readonly signal: AbortSignal;
-	// Readonly nativeOptions;
+import type {ChildProcess, SpawnOptions} from 'node:child_process';
+
+type StdioOption = Readonly<Exclude<SpawnOptions['stdio'], undefined>[number]>;
+type StdinOption = StdioOption | {readonly string?: string};
+
+export type Options = Omit<SpawnOptions, 'env' | 'stdio'> & Readonly<Partial<{
+	stdin: StdinOption;
+
+	stdout: StdioOption;
+
+	stderr: StdioOption;
+
+	stdio: SpawnOptions['stdio'] | readonly [StdinOption, ...readonly StdioOption[]];
+
+	preferLocal: boolean;
+
+	// Fixes issues with Remix and Next.js
+	// See https://github.com/sindresorhus/execa/pull/1141
+	env: Readonly<Partial<Record<string, string>>>;
+}>>;
+
+export type Result = {
+	stdout: string;
+
+	stderr: string;
+
+	output: string;
+
+	command: string;
+
+	durationMs: number;
 };
 
-// TODO: Finish this when the API is decided on.
-export function nanoSpawn(
-	command: string,
-	arguments: readonly string[],
-	options?: Options
-): Promise<void>;
+export type SubprocessError = Error & Result & {
+	exitCode?: number;
+
+	signalName?: string;
+};
+
+export type Subprocess = Promise<Result> & AsyncIterable<string> & {
+	nodeChildProcess: Promise<ChildProcess>;
+
+	stdout: AsyncIterable<string>;
+
+	stderr: AsyncIterable<string>;
+
+	pipe(file: string, arguments?: readonly string[], options?: Options): Subprocess;
+	pipe(file: string, options?: Options): Subprocess;
+};
+
+export default function nanoSpawn(file: string, arguments?: readonly string[], options?: Options): Subprocess;
+export default function nanoSpawn(file: string, options?: Options): Subprocess;
