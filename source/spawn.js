61560dabcd216e57d315dbaf814ad952e0a05914
ehmicky <ehmicky@gmail.com>
2024-09-08 19:38:41 +0100

    Add a code comment

--- source/spawn.js
+++ source/spawn.js
@@ -8,6 +8,7 @@ export const spawnSubprocess = async (file, commandArguments, options, context) 
 	try {
 		// When running `node`, keep the current Node version and CLI flags.
 		// Not applied with file paths to `.../node` since those indicate a clear intent to use a specific Node version.
+		// This also provides a way to opting out, e.g. using `process.execPath` instead of `node` to discard current CLI flags.
 		// Does not work with shebangs, but those don't work cross-platform anyway.
 		[file, commandArguments] = ['node', 'node.exe'].includes(file.toLowerCase())
 			? [process.execPath, [...process.execArgv.filter(flag => !flag.startsWith('--inspect')), ...commandArguments]]
