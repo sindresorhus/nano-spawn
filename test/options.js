b3edc9a441a0bac8ac71f5202f974ef7c19af9a5
ehmicky <ehmicky@gmail.com>
2025-05-04 14:13:01 +0100

    Fix tests

--- test/options.js
+++ test/options.js
@@ -37,9 +37,14 @@ test.serial('options.env augments process.env', async t => {
 	delete process.env.TWO;
 });
 
+// See https://github.com/sindresorhus/nano-spawn/issues/89
+const safeSpawn = (file, commandArguments, options) => options.shell
+	? spawn([file, ...commandArguments].join(' '), options)
+	: spawn(file, commandArguments, options);
+
 const testArgv0 = async (t, shell) => {
-	const {stdout} = await spawn(...nodePrintArgv0, {argv0: testString, shell});
-	t.is(stdout, shell ? process.execPath : testString);
+	const {stdout} = await safeSpawn(...nodePrintArgv0, {argv0: testString, shell});
+	t.is(stdout, shell ? nodePrintArgv0[0] : testString);
 };
 
 test('Can pass options.argv0', testArgv0, false);
