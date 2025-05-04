dcf36b652da3cd6c60ebc6ac3d6395989a62b613
ehmicky <ehmicky@gmail.com>
2025-05-04 15:20:28 +0100

    Fix `shell` option issue on Node 24

--- source/spawn.js
+++ source/spawn.js
@@ -14,7 +14,9 @@ export const spawnSubprocess = async (file, commandArguments, options, context) 
 			? [process.execPath, [...process.execArgv.filter(flag => !flag.startsWith('--inspect')), ...commandArguments]]
 			: [file, commandArguments];
 
-		const instance = spawn(...await applyForceShell(file, commandArguments, options));
+		[file, commandArguments, options] = await applyForceShell(file, commandArguments, options);
+		[file, commandArguments, options] = concatenateShell(file, commandArguments, options);
+		const instance = spawn(file, commandArguments, options);
 		bufferOutput(instance.stdout, context, 'stdout');
 		bufferOutput(instance.stderr, context, 'stderr');
 
@@ -29,6 +31,18 @@ export const spawnSubprocess = async (file, commandArguments, options, context) 
 		throw getResultError(error, {}, context);
 	}
 };
+
+// When the `shell` option is set, any command argument is concatenated as a single string by Node.js:
+// https://github.com/nodejs/node/blob/e38ce27f3ca0a65f68a31cedd984cddb927d4002/lib/child_process.js#L614-L624
+// However, since Node 24, it also prints a deprecation warning.
+// To avoid this warning, we perform that same operation before calling `node:child_process`.
+// Shells only understand strings, which is why Node.js performs that concatenation.
+// However, we rely on users splitting command arguments as an array.
+// For example, this allows us to easily detect whether the binary file is `node` or `node.exe`.
+// So we do want users to pass array of arguments even with `shell: true`, but we also want to avoid any warning.
+const concatenateShell = (file, commandArguments, options) => options.shell && commandArguments.length > 0
+	? [[file, ...commandArguments].join(' '), [], options]
+	: [file, commandArguments, options];
 
 const bufferOutput = (stream, {state}, streamName) => {
 	if (stream) {
