2f09674d600332d0a4abbef79d2be9c091f8eb0e
ehmicky <ehmicky@gmail.com>
2025-05-18 23:51:50 +0100

    Add support for Node 20.17.0

--- test/spawn.js
+++ test/spawn.js
@@ -33,7 +33,7 @@ test('Does not keep --inspect* Node flags', async t => {
 	t.false(stdout.includes(inspectCliFlag));
 });
 
-const TEST_NODE_VERSION = '20.18.0';
+const TEST_NODE_VERSION = '20.17.0';
 
 test.serial('Keeps Node version', async t => {
 	const {path: nodePath} = await getNode(TEST_NODE_VERSION);
